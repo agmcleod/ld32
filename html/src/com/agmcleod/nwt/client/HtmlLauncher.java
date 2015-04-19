3cb59bb54085c2a7337f1cfb66fd71be0f934ff6
Aaron McLeod <sircoolguy99@gmail.com>
2015-04-19 11:18:04 -0400

    fixed html launcher resolution

--- html/src/com/agmcleod/nwt/client/HtmlLauncher.java
+++ html/src/com/agmcleod/nwt/client/HtmlLauncher.java
@@ -7,13 +7,13 @@ import com.agmcleod.nwt.CoreGame;
 
 public class HtmlLauncher extends GwtApplication {
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
-        }
+    @Override
+    public GwtApplicationConfiguration getConfig () {
+        return new GwtApplicationConfiguration(704, 704);
+    }
 
-        @Override
-        public ApplicationListener getApplicationListener () {
-                return new CoreGame();
-        }
+    @Override
+    public ApplicationListener getApplicationListener () {
+        return new CoreGame();
+    }
 }
